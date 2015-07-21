79b99016623a19f4146d4a2fb3b8428f58c15b8b
Kate <lebedeva.kait@gmail.com>
2015-07-21 22:33:42 +0300

    init

---
+++ js/popup-jquery.js
@@ -0,0 +1,8 @@
+// PopupMobile Script
+
+
+// $.mobile.document.on("click", "open-popupArrow", function( evt) {
+// 	$("#popupArrow").popup("open", {x: evt.paegX, y: evt.pageY } );
+// 	evt.preventDefault();
+// });
+
